debeb5804f02829201bc56af0baf1da32cf37df1
aman863 <56828330+aman863@users.noreply.github.com>
2020-06-17 22:13:35 +0530

    update

--- utils/email.js
+++ utils/email.js
@@ -8,7 +8,7 @@ module.exports= class Email{
         this.to= user.email;
         this.firstName= user.name.split(" ")[0];
         this.url= url;
-        this.from=`Lebensstil <${process.env.EMAIL_FROM}>`;
+        this.from=`Lebensstil<support@lebesstil.in>`;
     }
     newTransport(){
         if(process.env.NODE_ENV==="production"){
